d46ad5e388827b93aa0eca6d5ac2d6aff3d72033
levits <milkis600@gmail.com>
2024-05-18 03:54:10 +0900

    init project

---
+++ src/main/java/com/book_rental_system/lib_rental/LibRentalApplication.java
@@ -0,0 +1,13 @@
+package com.book_rental_system.lib_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/book_rental_system/lib_rental/LibRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_rental_system.lib_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
